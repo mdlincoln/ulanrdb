85251f3d69da548090407fff2999c621c8f015c9
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-05-15 19:35:23 -0400

    Add package-level documentation

---
+++ R/ulanrdb-package.R
@@ -0,0 +1,9 @@
+#' ulanrdb package
+#'
+#' Ths package is meant to be used with the ulanr package. This package holds a
+#' local version of the ULAN database constructed by ulanr. See the
+#' \url{https://github.com/mdlincoln/ulanr} for more information.
+#'
+#' @name ulanrdb
+#' @docType package
+NULL
